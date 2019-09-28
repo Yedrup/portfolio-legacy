4b7eb82def19161b1fb41047c8a6ac4d0ae5b6eb
Yedrup <purdey.chambraud@gmail.com>
2019-09-28 07:52:07 +0200

    modify gulp build  to generate a www directory, add info about this portfolio becoming legacy

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-## MY PORTFOLIO
 
-Welcome on my first project.
+# LEGACY PORTFOLIO
+
+This is my former portfolio, its new location : https://purdeychambraud.com/portfolioLegacy
+New one will be soon released on https://purdeychambraud.com/ and add to Github.
+
+
+This was my first project.
 
 It is built with html5, CSS3 & CSS4, POSTCSS and gulp.
 
--- gulpfile.js
+++ gulpfile.js
@@ -9,42 +9,57 @@ let cleanCSS = require('gulp-clean-css');
 var atImport = require('postcss-import');
 var cssnext = require('postcss-cssnext');
 var svgFragments = require('postcss-svg-fragments');
+var cssnano = require('cssnano');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   gulp.watch('css/src/**/*.css', ['css_dev']);
-}); 
-
-//Delete CSS files before built
-gulp.task('cleanCssFiles', function() {
-  return del(['css/dest/*' ]);
 });
 
-//CSS production => minified
-gulp.task('css_prod',runSequence('cleanCssFiles'), function() {
+//Delete CSS files before built
+gulp.task('cleanCssFiles', function () {
+  return del(['css/dest/*']);
+});
+
+//clean www directory
+gulp.task('cleanWWW', function () {
+  return del(['www']);
+});
+
+gulp.task('css_prod', ['cleanWWW'] , function () {
   var processors = [
-  	atImport,
-    cssnext({browsers: 'last 3 versions'}),
-  	svgFragments
+    atImport,
+    cssnext({
+      browsers: 'last 3 versions'
+    }),
+    svgFragments,
+	  cssnano
   ];
   return gulp.src('css/src/_imports.css')
-    .pipe( postcss(processors) )
-    .pipe(rename('app.min.css'))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('css/dest'));
+    .pipe(postcss(processors))
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('www/css/'))
+    .pipe(gulp.src(['css/font-awesome/**','.htaccess','index.html','document/*', 'pages/**', 'img/**'], {
+      base: '.'
+    })
+    .pipe(gulp.dest('www/')))
 });
 
 //CSS dev => not minified
-gulp.task('css_dev', function() {
-  var processors = [
-  	atImport,
-    cssnext({browsers: 'last 3 versions'}),
-  	svgFragments
+gulp.task('css_dev', ['cleanWWW'],  function () {
+  var process = [
+    atImport,
+    cssnext({
+      browsers: 'last 3 versions'
+    }),
+    svgFragments
   ];
-  return gulp.src('css/src/_imports.css')
-    .pipe( postcss(processors) )
+  return (
+    gulp.src('css/src/_imports.css')
+    .pipe(postcss(process))
     .pipe(rename('app.css'))
-    .pipe(gulp.dest('css/dest'));
+    .pipe(gulp.dest('www/css/'))
+    .pipe(gulp.src(['./css/font-awesome/**','./index.html','./document/*', './pages/**', './img/**'], {
+      base: '.'
+    }).pipe(gulp.dest('www/')))
+  )
 });
-
-
-
